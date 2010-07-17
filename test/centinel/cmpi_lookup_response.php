59d1b7b5fa3773ded5bfd61501b668ab8077a2e5
akDeveloper <andreas.kollaros@gmail.com>
2010-07-17 19:46:30 +0300

    ADD: a table for cmpi_lookup responses

--- test/centinel/cmpi_lookup_response.php
+++ test/centinel/cmpi_lookup_response.php
@@ -5,7 +5,7 @@ require_once 'initialize_gateway.php';
 
 try {
   if ( isset($_POST['submit-cmpi-lookup']) ) {
-
+    $_SESSION['transaction_id'] = null;
     $cc = new Merchant_Billing_CreditCard( array(
         "first_name" => $_POST['first_name'],
         "last_name" => $_POST['last_name'],
@@ -22,7 +22,7 @@ try {
 
     $response = $gateway->lookup('1', $cc, $options);
 
-    if ( $response->success() && $response->enrolled == 'Y' ) {    
+    if ( $response->success() && $response->enrolled == 'Y' ) {
       $_SESSION['transaction_id'] = $response->authorization();
       echo '<HTML><BODY onload="document.frmLaunch.submit(); return false;">
         <FORM name="frmLaunch" method="POST" action="'.$response->acs_url.'">
@@ -33,7 +33,29 @@ try {
         </BODY></HTML>
         ';
     } else {
-      echo "<p>{$response->message()}</p>";
+      ?>
+  <table border="1" cellspacing="4" cellpadding="4">
+    <thead>
+      <tr>
+        <th>Enrolled</th>
+        <th>EciFlag</th>
+        <th>TransactionId</th>
+        <th>ErrorNo</th>
+        <th>ErrorDesc</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><?php echo $response->enrolled ?></td>
+        <td><?php echo $response->eci_flag ?></td>
+        <td><?php echo $response->transaction_id ?></td>
+        <td><?php echo $response->error_no ?></td>
+        <td<?php echo $response->error_desc ?></td>
+      </tr>
+    </tbody>
+  </table>
+  <p><a href="index.php">Return to test page</a></p>
+      <?php
     }
   } else {
     if ( isset($_POST['PaReq']) &&  $_POST['PaReq'] == "" ) {
