9864928716affa16a47d7e02712ed6765c805104
Tom <tom@mail.wordpress.localhost>
2011-05-05 01:46:41 +0800

    Begun porting PayFlow

---
+++ lib/merchant/billing/gateways/payflow/PayflowResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+class Merchant_Billing_PayflowResponse extends Merchant_Billing_Response
+{
+    function profileId()
+    {
+        return $this->params['profileId'];
+    }
+
+    function paymentHistory()
+    {
+        if(is_array($this->params['rpPaymentResult']))
+            return $this->params['rpPaymentResult'];
+        else
+            return array();
+    }
+}
