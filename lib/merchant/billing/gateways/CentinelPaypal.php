e434faf7de97ef0995c98dc4fbb39ce6d1efd2d6
Tom Maguire <tom@musicglue.com>
2012-06-28 10:12:35 +0100

    revert to old Merchant_Billing_Centinel LIVE_URL + add Merchant_Billing_Centinel_Paypal for centinel accounts registered via Paypal

---
+++ lib/merchant/billing/gateways/CentinelPaypal.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once 'Centinel.php';
+
+class Merchant_Billing_Centinel_Paypal extends Merchant_Billing_Centinel {
+  const LIVE_URL = 'https://paypal.cardinalcommerce.com/maps/txns.asp';
+}
+
+?>
