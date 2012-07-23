01a9da267950542f9983db046f2aabd4295c7562
Andreas Kollaros <andreas.kollaros@gmail.com>
2012-07-23 20:51:04 +0300

    add autoload and TestCase class

--- unit_tests/config.php
+++ unit_tests/config.php
@@ -1,5 +1,6 @@
 <?php
-require_once dirname(__FILE__) . '/../lib/merchant.php';
+require_once dirname(__FILE__) . '/../lib/autoload.php';
 require_once dirname(__FILE__) . '/../test/login.php';
 require_once dirname(__FILE__) . '/AktiveMerchant/Mock/Request.php';
+require_once dirname(__FILE__) . '/AktiveMerchant/TestCase.php';
 ?>
