203ded406c111bb80b182d96a3cae54dc8a9ba8c
Andreas Kollaros <andreas.kollaros@gmail.com>
2012-07-23 21:24:26 +0300

    fix paypal express example and add order items to pass to paypal express

--- test/paypal_express/index.php
+++ test/paypal_express/index.php
@@ -1,62 +1,81 @@
 <?php
-require_once('../../lib/merchant.php');
+require_once('../../lib/autoload.php');
 require_once('../login.php');
 
-Merchant_Billing_Base::mode('test');
+use AktiveMerchant\Billing\Base;
+use AktiveMerchant\Billing\Gateways\PaypalExpress;
 
-$gateway = new Merchant_Billing_PaypalExpress( array(
-  'login' => PAYPAL_LOGIN,
-  'password' => PAYPAL_PASS,
-  'signature' => PAYPAL_SIG,
-  'currency' => 'EUR'
-  )
+Base::mode('test');
+
+$gateway = new PaypalExpress(
+    array(
+        'login' => PAYPAL_LOGIN,
+        'password' => PAYPAL_PASS,
+        'signature' => PAYPAL_SIG,
+        'currency' => 'USD'
+    )
 );
 
 try {
 
-  if ( isset($_GET['pay']) ) {
+    if ( isset($_GET['pay']) ) {
+        $options = array(
+            'return_url' => 'http://localhost/Aktive-Merchant/test/paypal_express/index.php',
+            'cancel_return_url' => 'http://localhost/Aktive-Merchant/test/paypal_express/index.php?cancel=1',
+            'items' => array(
+                array(
+                    'name' => 'Shirt',
+                    'description' => 'Blue shirt',
+                    'unit_price' => 10,
+                    'quantity' => 1,
+                    'id' => '1245345'
+                ),
+                array(
+                    'name' => 'Shirt',
+                    'description' => 'White shirt',
+                    'unit_price' => 12,
+                    'quantity' => 3,
+                    'id' => '124242345'
+                ),
+            )
+        );
+        $response = $gateway->setupPurchase($_POST['amount'],$options);
 
-    $response = $gateway->setup_purchase($_POST['amount'], array(
-      'return_url' => 'http://localhost/Aktive-Merchant/test/paypal_express/index.php',
-      'cancel_return_url' => 'http://localhost/Aktive-Merchant/test/paypal_express/index.php?cancel=1',
-      )
-    );
 
+        die ( header('Location: '. $gateway->urlForToken( $response->token() )) );
 
-    die ( header('Location: '. $gateway->url_for_token( $response->token() )) );
+    } elseif (isset($_GET['cancel'])) {
 
-  } elseif (isset($_GET['cancel'])) {
+        echo 'Transaction Canceled!<br />';
 
-    echo 'Transaction Canceled!<br />';
+    } elseif (isset( $_GET['token'] ) ) {
 
-  } elseif (isset( $_GET['token'] ) ) {
+        $response = $gateway->get_details_for( $_GET['token'], $_GET['PayerID']);
 
-    $response = $gateway->get_details_for( $_GET['token'], $_GET['PayerID']);
+        /**
+         * You can modify transaction amount according to paypal ship address
+         * or even render a form to allow customer choose shipping methods and
+         * additional costs.
+         * NOTE: if you execute $gateway->authorize() or $gateway->purchase() to a
+         * different page than you executed $gateway->get_details_for()
+         * make sure you have store somewhere token and payer_id values
+         * ex. $_SESSION or Database
+         */
 
-    /**
-     * You can modify transaction amount according to paypal ship address
-     * or even render a form to allow customer choose shipping methods and
-     * additional costs.
-     * NOTE: if you execute $gateway->authorize() or $gateway->purchase() to a
-     * different page than you executed $gateway->get_details_for()
-     * make sure you have store somewhere token and payer_id values
-     * ex. $_SESSION or Database
-     */
-
-    $response = $gateway->purchase($response->amount());
-    if ( $response->success() ) {
-      echo 'Success payment!';
-    } else {
-      echo $response->message();
+        $response = $gateway->purchase($response->amount());
+        if ( $response->success() ) {
+            echo 'Success payment!';
+        } else {
+            echo $response->message();
+        }
     }
-  }
 } catch (Exception $exc) {
-  echo $exc->getMessage();
+    echo $exc->getMessage();
 }
 
 ?>
 <form method="post" action="/Aktive-Merchant/test/paypal_express/index.php?pay=1">
-  <label for="amount">Amount</label>
-  <input type="text" name="amount" value="1" id="amount" />
-  <input type="submit" />
+    <label for="amount">Amount</label>
+    <input type="text" name="amount" value="46" id="amount" />
+    <input type="submit" />
 </form>
