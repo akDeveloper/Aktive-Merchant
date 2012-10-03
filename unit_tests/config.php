45b8294c40c075c05ea741be075d40fa4b3645b7
Andreas Kollaros <andreas.kollaros@gmail.com>
2012-10-04 01:19:32 +0300

    move autoload files outside lib folder

--- unit_tests/config.php
+++ unit_tests/config.php
@@ -1,6 +1,6 @@
 <?php
-require_once dirname(__FILE__) . '/../lib/autoload.php';
-require_once dirname(__FILE__) . '/../test/login.php';
-require_once dirname(__FILE__) . '/AktiveMerchant/Mock/Request.php';
-require_once dirname(__FILE__) . '/AktiveMerchant/TestCase.php';
+require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . '/../test/login.php';
+require_once __DIR__ . '/AktiveMerchant/Mock/Request.php';
+require_once __DIR__ . '/AktiveMerchant/TestCase.php';
 ?>
