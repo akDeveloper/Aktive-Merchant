dc5d66415efdead388f5bb8ffa922b4d7cac06ec
Andreas Kollaros <andreas.kollaros@gmail.com>
2012-12-29 21:55:51 +0200

    fix directory path for autoload

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('UTC');
 
 require_once 'SplClassLoader.php';
-$loader = new SplClassLoader('AktiveMerchant', __DIR__ . '/lib/');
+$loader = new SplClassLoader('AktiveMerchant', __DIR__ . '/lib');
 $loader->register();
 
 ?>
