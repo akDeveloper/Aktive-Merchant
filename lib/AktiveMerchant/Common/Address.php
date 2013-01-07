9e73baf5d028ff1aff815d477935ee693e884bf2
Andreas Kollaros <andreas.kollaros@gmail.com>
2013-01-07 11:54:26 +0200

    add Address class

---
+++ lib/AktiveMerchant/Common/Address.php
@@ -0,0 +1,125 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+namespace AktiveMerchant\Common;
+
+/**
+ * Address class allows mapping user defined fields of an address array to 
+ * corresponding fields of a gateway.
+ * 
+ * <code>
+ *      $user_address = array(
+ *          'city_name' => 'Athens',
+ *          'country_name' => 'Greece',
+ *      );
+ *      
+ *      $address = new Address($user_address); // create new instance
+ *
+ *      $address->map('city', 'GATEWAY_CITY')    // maps the city field.
+ *          ->map('country', 'GATEWAY_COUNTRY'); // maps the country field.
+ *
+ *      $fields = $address->getMappedFields();
+ *      print_r($fields);
+ *      // Output:
+ *      // Array(
+ *      //      'GATEWAY_CITY' => 'Athens',
+ *      //      'GATEWAY_COUNTRY' => 'Greece' 
+ *      // )
+ *
+ *
+ * </code>
+ *
+ * 
+ * @package Aktive-Merchant
+ * @author  Andreas Kollaros 
+ * @license http://opensource.org/licenses/mit-license.php
+ */
+class Address
+{
+
+    /**
+     * An array or Options class withe user definded fields of an address.
+     * 
+     * @var array|Options
+     */
+    protected $fields = array();
+
+    /**
+     * An array with mapped references from user defined address and gateway.
+     *
+     * @var array
+     */
+    protected $mappings = array();
+
+    public function __construct($fields = array())
+    {
+        $this->fields = $fields;
+    }
+
+    /**
+     * Maps an address field from user to the corresponding field from the 
+     * gateway.  
+     * 
+     * @param string $user_field 
+     * @param string $gateway_field 
+     * 
+     * @access public
+     *
+     * @return Address
+     */
+    public function map($user_field, $gateway_field)
+    {
+        $this->mappings[$user_field] = $gateway_field;
+
+        return $this;
+    }
+
+    /**
+     * Sets an array with address fields
+     * 
+     * @param array $fields 
+     * 
+     * @access public
+     *
+     * @return void
+     */
+    public function setFields($fields = array())
+    {
+        $this->fields = $fields;
+    }
+
+    /**
+     * Gets an array with address fields.
+     * 
+     * @access public
+     *
+     * @return array
+     */
+    public function getFields()
+    {
+        return $this->fields;
+    }
+
+    /**
+     * Gets an array with mapped address fields.
+     *
+     * @access public
+     * 
+     * @return array
+     */
+    public function getMappedFields()
+    {
+        $mapped = array();
+
+        foreach ($this->fields as $key=>$value) {
+        
+            if (array_key_exists($key, $this->mappings)) {
+                $map = $this->mappings[$key];
+                $mapped[$map] = $value;
+            }
+        }
+
+        return $mapped;
+    }
+}
---
+++ unit_tests/AktiveMerchant/Common/AddressTest.php
@@ -0,0 +1,51 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+namespace AktiveMerchant\Common;
+
+/**
+ * AddressTest class.
+ *
+ * @package Aktive-Merchant
+ * @author  Andreas Kollaros
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ *
+ */
+
+require_once '../autoload.php';
+
+class AddressTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMapping()
+    {
+        $user_address = array(
+            'city_name' => 'Athens',
+            'country_name' => 'Greece',
+            'state' => 'Attiki',
+            'postal_code' => '23456',
+            'address_line_1' => 'Address 7 Street',
+            'phone_number' => '30210123456',
+            'fullname' => 'Andreas Kollaros', 
+        );
+
+        $address = new Address();
+
+        $address->setFields($user_address);
+
+        $address->map('fullname', 'SHIPTONAME')
+            ->map('phone_number', 'PHONENUM')
+            ->map('city_name', 'SHIPTOCITY')
+            ->map('address_line_1', 'SHIPTOSTREET')
+            ->map('state', 'SHIPTOSTATE')
+            ->map('country_name', 'SHIPTOCOUNTRY')
+            ->map('postal_code', 'SHIPTOZIP');
+        
+        $fields = $address->getMappedFields();
+
+        $this->assertEquals('Attiki', $fields['SHIPTOSTATE']);
+        $this->assertEquals('Athens', $fields['SHIPTOCITY']);
+        $this->assertEquals('23456', $fields['SHIPTOZIP']);
+
+    }
+}
