ddb2df58aa23c46b3e9c3b6931904ba11603921e
Michael Gall <michael@wakeless.net>
2013-01-20 23:59:34 +0700

    Need to remove these to make it work. Probably not ideal

--- lib/AktiveMerchant/Http/Request.php
+++ lib/AktiveMerchant/Http/Request.php
@@ -146,12 +146,9 @@ class Request implements RequestInterface
             curl_setopt($curl, CURLOPT_HEADER, 1);
             curl_setopt($curl, CURLINFO_HEADER_OUT, 1);
             curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, $this->allow_unsafe_ssl);
-            curl_setopt($curl, CURLOPT_SSLVERSION, 3);
-            curl_setopt($curl, CURLOPT_SSL_CIPHER_LIST, 'RC4-MD5');
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // $this->allow_unsafe_ssl);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); // $this->allow_unsafe_ssl);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($curl, CURLOPT_FORBID_REUSE, 1);
-            curl_setopt($curl, CURLOPT_FRESH_CONNECT, 1);
             curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $this->connect_timeout);
             curl_setopt($curl, CURLOPT_TIMEOUT, $this->request_timeout);
             curl_setopt($curl, CURLOPT_USERAGENT, $this->user_agent);
