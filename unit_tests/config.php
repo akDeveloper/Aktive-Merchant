ce450781990788c74f7ae763c6eaacd35ce183f5
akDeveloper <andreas.kollaros@gmail.com>
2014-05-08 22:15:32 +0300

    fix  autoload

--- unit_tests/config.php
+++ unit_tests/config.php
@@ -1,5 +1,4 @@
 <?php
-require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/AktiveMerchant/Mock/Request.php';
 require_once __DIR__ . '/AktiveMerchant/TestCase.php';
-?>
