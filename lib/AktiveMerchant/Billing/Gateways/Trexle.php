260e9d4aff14bf6c049a90f53cc30c99687099bf
Hossam Hossny <hossam@trexle.com>
2017-05-02 09:12:22 +0200

    improvements

--- lib/AktiveMerchant/Billing/Gateways/Trexle.php
+++ lib/AktiveMerchant/Billing/Gateways/Trexle.php
@@ -21,11 +21,11 @@ use AktiveMerchant\Http\RequestInterface;
  *  $options = ['secret_key' => <yoursecretkey>];
  *  $gateway = AktiveMerchant\Billing\Base::gateway('trexle', $options);
  *  // Setup customer token or card token or card data.
- *  $card = new CreditCard(['token' => 'cus_xxxxxxx']); // Customer token
- *  $card = new CreditCard(['token' => 'card_xxxxxxx']); // Card token
+ *  $card = new CreditCard(['token' => 'token_xxxxxxx']); // Customer token
+ *  $card = new CreditCard(['token' => 'token_xxxxxxx']); // Card token
  *  $card = new CreditCard([
  *        "first_name" => "John",
- *        "last_name" => "Doe",
+ *        "last_name" => "Milwood",
  *        "number" => "4242424242424242",
  *        "month" => "01",
  *        "year" => "17",
@@ -38,7 +38,7 @@ use AktiveMerchant\Http\RequestInterface;
  *  // Get unique reference from transaction for future use.
  *  $authorization = $response->authorization();
  *
- * @author Andreas Kollaros <andreas@larium.net>
+ * @author Hossam Hossny <hossamhossnyar@gmail.com>
  * @license MIT License http://www.opensource.org/licenses/mit-license.php
  */
 class Trexle extends Gateway implements
